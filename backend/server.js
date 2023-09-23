912dd1384a8584b217895665418a6041dbd43ce1
VarunHujan1403 <100893691+VarunHujan1403@users.noreply.github.com>
2023-09-23 01:12:50 -0700

    commit

---
+++ backend/server.js
@@ -0,0 +1,111 @@
+import cors from "cors";
+import express from "express";
+import axios from "axios";
+
+const app = express();
+app.use(cors());
+
+const api_data = {
+  companyName: "",
+  clientID: "",
+  ownerName: "",
+  ownerEmail: "",
+  rollNo: "",
+  clientSecret: "",
+};
+let auth_token = "";
+async function getToken(api_data) {
+  try {
+    const response = await axios.post(
+      "http://20.244.56.144/train/auth",
+      api_data
+    );
+    const token = response.data.access_token;
+    return token;
+  } catch (error) {
+    console.error("Error generatin token:", error);
+    throw error;
+  }
+}
+setInterval(async () => {
+  auth_token = await getToken(api_data);
+}, 35000);
+
+async function fetchTrains() {
+  const AUTH_TOKEN = await getToken(api_data);
+  try {
+    const headers = {
+      Authorization: `Bearer ${AUTH_TOKEN}`,
+    };
+    const res = await axios.get("http://20.244.56.144/train/trains", {
+      headers,
+    });
+    const trainsData = res.data;
+    return trainsData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+function sortingTrains(trains_data) {
+  function customSort(a, b) {
+    const priceComp = a.price.AC - b.price.AC;
+    if (priceComp !== 0) {
+      return priceComp;
+    }
+    const seatsAvailableComp = b.seatsAvailable.AC - a.seatsAvailable.AC;
+    if (seatsAvailableComp !== 0) {
+      return seatsAvailableComp;
+    }
+    const departureTimeA = a.departureTime.Hours * 60 + a.departureTime.Minutes;
+    const departureTimeB = b.departureTime.Hours * 60 + b.departureTime.Minutes;
+    return departureTimeB - departureTimeA;
+  }
+  for (let i = 0; i < trains_data.length - 1; i++) {
+    for (let j = 0; j < trains_data.length - i - 1; j++) {
+      if (customSort(trains_data[j], trains_data[j + 1]) > 0) {
+        const temp = trains_data[j];
+        trains_data[j] = trains_data[j + 1];
+        trains_data[j + 1] = temp;
+      }
+    }
+  }
+  const sortedTrains = [];
+  for (const train of trains_data) {
+    if (train.departureTime.Minutes > 30) {
+      sortedTrains.push(train);
+    }
+  }return sortedTrains;
+}
+app.get("/", async (req, res) => {
+  try {
+    const trainsData = await fetchTrains();
+    const sortedTrains = sortingTrains(trainsData);
+    res.json(sortedTrains);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+app.get("/getTrain/:trainId", async (req, res) => {
+  try {
+    const train_num = req.params.trainId;
+    const AUTH_TKN = await getToken(api_data);
+
+    const headers = {
+      Authorization: `Bearer ${AUTH_TKN}`,
+    };
+
+    const train_data = await axios.get(
+      `http://20.244.56.144/train/trains/2344`,
+      { headers }
+    );
+    res.json(train_data.data);
+  } catch (error) {
+    console.error("Unable to fetch data", error);
+    res.status(200).json({ error: "Something went wrong" });
+  }
+});
+app.listen(3000, () => {
+  console.log("Server is running on port http://localhost:8000");
+});
